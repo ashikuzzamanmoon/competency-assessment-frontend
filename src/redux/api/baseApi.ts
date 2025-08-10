6df72ce068e39c43732687d77191a718904a6d64
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2025-08-10 21:25:29 +0600

    fix: update backend api url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import type { RootState } from "../store";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://competency-assessment-backend.vercel.app",
+    baseUrl: "https://competency-assessment-backend.vercel.app/api/v1",
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
       if (token) {
